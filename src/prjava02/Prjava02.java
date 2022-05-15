a3242966f07ede3de319ffede744b83aeb68767b
Arnau Font <arnaufontbargallo@Hola.local>
2022-05-16 00:13:26 +0200

    Treballant amb ordres Git i branques: Versió 1.2 del projecte prjavaFont02

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -27,6 +27,7 @@ public class Prjava02 {
             System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
             System.out.println("Versió OS: " + System.getProperty("os.version"));
             System.out.println("Creació d'una branca del projecte prjavaFont02");
+            System.out.println("Afegint més codi a la branca 00 del projecte prjavaFont02");
         }
         catch (IOException e) {
 		System.out.println("Exception occurred");
